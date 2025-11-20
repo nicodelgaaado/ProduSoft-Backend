5ce4faecf050d28cf33554a46c72cd211248c182
nicolas <nicolas.delgadop@campusuuc.edu.co>
2025-11-20 01:42:49 -0500

    workflow backend

---
+++ backend/src/test/java/com/produsoft/workflow/WorkflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produsoft.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
