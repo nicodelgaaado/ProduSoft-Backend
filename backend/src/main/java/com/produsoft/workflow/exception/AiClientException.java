5a7e510ebe4c32317d8b612b544f34ae5ce6bad3
nicolas <nicolas.delgadop@campusuuc.edu.co>
2025-11-20 01:40:04 -0500

    ai client exception

---
+++ backend/src/main/java/com/produsoft/workflow/exception/AiClientException.java
@@ -0,0 +1,12 @@
+package com.produsoft.workflow.exception;
+
+public class AiClientException extends RuntimeException {
+
+    public AiClientException(String message) {
+        super(message);
+    }
+
+    public AiClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
